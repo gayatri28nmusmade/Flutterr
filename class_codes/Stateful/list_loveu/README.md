4b2244dde36685a50bc38c760ee6a619d69f708d
gayatri28nmusmade <115856392+gayatri28nmusmade@users.noreply.github.com>
2024-10-01 23:55:44 +0530

    Delete class_codes/Stateful/

--- class_codes/Stateful/list_loveu/README.md
+++
@@ -1,3 +0,0 @@
-# list_loveu
-
-A new Flutter project.
--- class_codes/Stateful/list_loveu/android/app/src/main/kotlin/com/example/list_loveu/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.list_loveu
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
